6e1435cb789900a46138e74d788ff445c3d7d947
tankh99 <khanghou99@gmail.com>
2024-06-22 15:57:26 +0800

    feat: setup tailwind

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ This template should help get you started developing with Tauri, React and Types
 ## Recommended IDE Setup
 
 - [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
+
+# Setting up Tailwind
+1. Follow Tailwind's React setup instructions
+2. At the end, make sure to add a postcss.config.js! This is crucial as without it, tailwind won't even be regisered properly
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { internalIpV4 } from "internal-ip";
+import { resolve } from 'path';
 
-// @ts-expect-error process is a nodejs global
 const mobile = !!/android|ios/.exec(process.env.TAURI_ENV_PLATFORM);
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [react()],
-
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
   // 1. prevent vite from obscuring rust errors

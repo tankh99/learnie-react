cfe5dccec1424a6b188223e1fcaaaa4bb3bd5648
tankh99 <khanghou99@gmail.com>
2024-06-23 10:00:22 +0800

    chore :adding temp notif logic

--- src/pages/notes/index.tsx
+++ src/pages/notes/index.tsx
@@ -4,6 +4,7 @@ import { Button } from '@/components/ui/button';
 import { Note } from '@/types/Note';
 import { QueryClient, useQuery } from '@tanstack/react-query'
 import { Link } from 'react-router-dom';
+import { isPermissionGranted, requestPermission, sendNotification } from '@tauri-apps/plugin-notification'
 
 export const loader = (queryClient: QueryClient) => async ({}) => {
   const query = getNotesQuery();
@@ -11,10 +12,25 @@ export const loader = (queryClient: QueryClient) => async ({}) => {
 }
 
 export default function NotesPage() {
-  const {data: notes, isPending } = useQuery(getNotesQuery())
+  const {data: notes } = useQuery(getNotesQuery())
+
+  const notify = async () => {
+    let permissionGranted = await isPermissionGranted()
+    if (!permissionGranted) {
+      const permission = await requestPermission();
+      permissionGranted = permission === "granted";
+    }
+    if (permissionGranted) {
+      console.log("sending notif")
+      sendNotification({title: "Spngeob", body: "Jellyfish"});
+    }
+  }
 
   return (
     <div>
+      <Button onClick={() => notify()}>
+        Test Notif
+      </Button>
       <Link to={"/notes/create"}>
         <Button>
           Create Note

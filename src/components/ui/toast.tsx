35cb17bb07ac127e6153810e58da5716de3a2783
tankh99 <khanghou99@gmail.com>
2024-06-30 15:39:19 +0800

    fix: destructive toast variant

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -29,7 +29,7 @@ const toastVariants = cva(
       variant: {
         default: "border bg-background text-foreground",
         destructive:
-          "destructive group border-destructive bg-destructive text-destructive-foreground",
+          "destructive group border-destructive bg-destructive-foreground text-destructive",
         success: "success group border-success bg-success-foreground",
       },
     },
